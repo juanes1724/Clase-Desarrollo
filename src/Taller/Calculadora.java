d64527093d2294a702c12c1445cf4f5bb40a624a
juan eteban <alvarado.juan.1724@eam.edu.co>
2025-02-20 18:36:05 -0500

    funcion division

--- src/Taller/Calculadora.java
+++ src/Taller/Calculadora.java
@@ -10,6 +10,11 @@ public class Calculadora{
     System.out.println("Suma: " + sumar(num1, num2));
     System.out.println("Resta: " + restar(num1, num2));
     System.out.println("Multiplicación: " + multiplicar(num1, num2));
+    if (num2 == 0) {
+        System.out.println("Error: División por 0");
+    } else {
+        System.out.println("División: " + dividir(num1, num2));
+    }
 
  }
 }
