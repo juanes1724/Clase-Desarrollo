a66da85d9e5ba7ea339b5d06665d1bac50cd21b1
juan eteban <alvarado.juan.1724@eam.edu.co>
2025-02-20 18:33:04 -0500

    funcion resta

--- src/Taller/Calculadora.java
+++ src/Taller/Calculadora.java
@@ -8,6 +8,7 @@ public class Calculadora{
     double num2 = scanner.nextDouble();
     
     System.out.println("Suma: " + sumar(num1, num2));
+    System.out.println("Resta: " + restar(num1, num2));
 
  }
 }
